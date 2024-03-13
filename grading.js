98943dc4ecd748bc7a9a6a05ea098564c209a1b3
Olu Cashie <macotewi@gmail.com>
2024-03-13 11:22:34 +0100

    ok

--- grading.js
+++ grading.js
@@ -1,25 +1,49 @@
+
 function calculateGrade() {
     const score = document.getElementById("score").value;
-  
-    let grade = "";
+   
+    var grade = Number(score.value)
+    var songfail = new Audio ("music/failure.mp3")
+    var songpass = new Audio ("music/good.mp3")
+    var songaverage = new Audio ("music/average.mp3")
+    var songcredit = new Audio ("music/credit.mp3")
+    var songgood = new Audio ("music/pass.mp3")
+    var songexcellent = new Audio ("music/clapping.mp3") 
+
+
+
+    // let grade = "";
     if (score == ""){
       grade = "Empty ❗";
-    } else if (score >=  90) {
+    }else if (score>100){
+      grade = "Out of Score"
+    } else if(score <0){
+grade = "Lower than the score"
+    }
+    else if (score >=  90) {
       grade = "A1🙌";
+      songexcellent.play()
     } else if (score >= 80) {
       grade = "B2👏";
+      songgood.play()
     } else if (score >= 70) {
       grade = "B3🤝";
+      songpass.play()
     } else if (score >= 65) {
       grade = "C4😏";
+      songcredit.play()
     }else if (score >= 60) {
       grade = "C5🤨";
+      songgood.play()
     }else if (score >= 55) {
       grade = "C6😒";
+      songaverage.play()
     }else if (score >= 45) {
       grade = "D7🙁";
+      songfail.play()
     }else if (score <= 40 || score == 0) {
       grade = "F9😢";
+      songfail.play()
     }
 
     const result = document.getElementById("result");
